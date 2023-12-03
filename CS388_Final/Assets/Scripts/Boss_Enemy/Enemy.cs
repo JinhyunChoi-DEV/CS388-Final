8e068591f7adf33ec34d2be8473585e6fbd60b19
Hyosang Jeong <70678504+Hyosang-Jeong@users.noreply.github.com>
2023-12-02 22:29:04 -0800

    Add navmesh

--- CS388_Final/Assets/Scripts/Boss_Enemy/Enemy.cs
+++ CS388_Final/Assets/Scripts/Boss_Enemy/Enemy.cs
@@ -1,39 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
+using UnityEngine.UI;
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
     public GameObject Player;
     public GameObject bullet;
+    public LayerMask obstacleLayer;
+    private NavMeshAgent agent;
+    public Slider hpbar;
     private Vector2 DirToPlayer;
-    private Vector2 Speed = new Vector2(2f,2f);
-    private float MinDistanceToPlayer = 3;
+    private float MinDistanceToPlayer = 8;
     private float timer = 0;
+    private float CurHp = 100f;
+    private float MaxHP = 100f;
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        DirToPlayer =( Player.gameObject.transform.position - gameObject.transform.position);
-        if (DirToPlayer.magnitude > MinDistanceToPlayer)
+        DirToPlayer = (Player.transform.position - transform.position);
+
+        hpbar.value = CurHp / MaxHP;
+
+        float distanceToPlayer = DirToPlayer.magnitude;
+
+        if (distanceToPlayer > MinDistanceToPlayer)
         {
-            GetComponent<Rigidbody2D>().velocity = Speed * DirToPlayer.normalized;
+            agent.SetDestination(Player.transform.position);
         }
         else
         {
-            timer += Time.deltaTime;
-            if(timer >2f)
+            if (IsObstacleBetweenEnemyAndPlayer() == false)
             {
-                GenerateBullet();
-                timer = 0;
+                timer += Time.deltaTime;
+                if (timer > 2f)
+                {
+                    GenerateBullet();
+                    timer = 0;
+                }
+                agent.ResetPath();
             }
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
+            else
+            {
+                agent.SetDestination(Player.transform.position);
+            }
         }
+
+        if(CurHp <= 0)
+        {
+            Destroy(gameObject);
+        }
+
     }
 
     void GenerateBullet()
@@ -41,5 +64,25 @@ public class Enemy : MonoBehaviour
         GameObject bullet_ = Instantiate(bullet, gameObject.transform);
         bullet_.transform.rotation = Quaternion.FromToRotation(Vector3.up, DirToPlayer);
         bullet_.GetComponent<Rigidbody2D>().velocity = new Vector2(10f, 10f) * DirToPlayer.normalized;
+    }
+
+    bool IsObstacleBetweenEnemyAndPlayer()
+    {
+        float distanceToPlayer = DirToPlayer.magnitude;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, (Player.transform.position - transform.position).normalized, distanceToPlayer, obstacleLayer);
+
+        if (hit.collider != null)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Bullet"))
+        {
+            CurHp -= 20;
+        }
     }
 }
