ec1e23f688e9b7fc97feef0665531573ede1a216
Sunwoo Lee <sunwoo9078@gmail.com>
2023-12-03 23:59:57 -0800

    Adding sound2

--- CS388_Final/Assets/Scripts/MainMenu/MainMenu.cs
+++ CS388_Final/Assets/Scripts/MainMenu/MainMenu.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
     public Button startButton;
+    public AudioClip Button_Clip;
 
     void Start()
     {
@@ -21,11 +22,13 @@ public class MainMenu : MonoBehaviour
 
     public void OnClickStart()
     {
+        SoundManager.instance.SFXPlay("Button", Button_Clip);
         SceneManager.LoadScene("PlayableScene");
     }
 
     public void OnClickEnd()
     {
+        SoundManager.instance.SFXPlay("Button", Button_Clip);
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #else
