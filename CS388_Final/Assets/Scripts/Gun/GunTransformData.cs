b67a85909eeac728b3a37ee19eec77a34710bbbf
Jinhyun Choi <jinhyun.choi@digipen.edu>
2023-12-19 20:23:40 -0800

    remove nintendo SDK codes

--- CS388_Final/Assets/Scripts/Gun/GunTransformData.cs
+++ CS388_Final/Assets/Scripts/Gun/GunTransformData.cs
@@ -1,3 +1,15 @@
+/*
+* Name: GunTransformData
+* Author: Jinhyun Choi
+* Copyright © 2023 DigiPen (USA) LLC. and its owners. All Rights
+Reserved.
+No parts of this publication may be copied or distributed,
+transmitted, transcribed, stored in a retrieval system, or
+translated into any human or computer language without the
+express written permission of DigiPen (USA) LLC., 9931 Willows
+Road NE, Redmond, WA 98052, USA.
+ */
+
 using UnityEngine;
 
 public class GunTransformData : MonoBehaviour
--- CS388_Final/Assets/Scripts/UI/PlayerDeadUI.cs
+++ CS388_Final/Assets/Scripts/UI/PlayerDeadUI.cs
@@ -1,3 +1,14 @@
+/*
+* Name: PlayerDeadUI
+* Author: Jinhyun Choi
+* Copyright © 2023 DigiPen (USA) LLC. and its owners. All Rights
+Reserved.
+No parts of this publication may be copied or distributed,
+transmitted, transcribed, stored in a retrieval system, or
+translated into any human or computer language without the
+express written permission of DigiPen (USA) LLC., 9931 Willows
+Road NE, Redmond, WA 98052, USA.
+ */
 using System.Collections;
 using NUnit.Framework.Constraints;
 using UnityEngine;
